88476779d26218919042b73191522e9907986d8c
Leonardo Gomes <leonardog.bat@gmail.com>
2024-10-22 22:01:34 -0300

    feat: create `Footer` component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Made by Leonardo Gomes Batista © 2024
+    </footer>
+  );
+}
