bf8c3de4ad91c88a334c233ceee024ab083a041a
Leonardo Gomes <leonardog.bat@gmail.com>
2024-11-02 12:48:25 -0300

    feat: use `getPosts` from blog fetchers utils to retrieve posts data

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,23 +1,23 @@
+import Link from 'next/link';
+import { getPosts } from '@/utils/blogFetchers';
+import { formatISODateToLocaleString } from '@/utils/date';
 import styles from './page.module.css';
 
-export default function Blog() {
+export default async function Blog() {
+  const posts = await getPosts();
+
   return (
     <section className={styles.blog}>
-      <h1>Blog</h1>
-
-      <article className={styles.post}>
-        <h2>Docker and Microservices</h2>
-        <pre>
-          <time>Outubro 23, 2024</time> — 3min — DevOps
-        </pre>
-      </article>
-
-      <article className={styles.post}>
-        <h2>React Native 0.75</h2>
-        <pre>
-          <time>Outubro 23, 2024</time> — 3min — DevOps
-        </pre>
-      </article>
+      {posts.map((post) => (
+        <article key={post.slug} className={styles.post}>
+          <Link href={`/blog/${post.slug}`}>
+            <h3>{post.metadata.title}</h3>
+          </Link>
+          <pre>
+            <time>{formatISODateToLocaleString(post.metadata.publish_date)}</time>
+          </pre>
+        </article>
+      ))}
     </section>
   );
 }
