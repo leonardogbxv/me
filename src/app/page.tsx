94671d539c55a2fe46055f789a73bc1ad8d4732f
Leonardo Gomes <leonardog.bat@gmail.com>
2024-11-02 12:45:16 -0300

    refactor: replace `<a>` tags with next link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ import { Metadata } from 'next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
-  title: 'Leonardo G. Batista',
+  title: 'Leonardo Gomes Batista',
   description: 'leonardogbxv\'s portfolio & blog'
 };
 
@@ -14,41 +15,41 @@ export default function Home() {
   return (
     <>
       <section className={styles.headliner}>
-        <a href="https://github.com/leonardogbxv" target="_blank">
+        <Link href="https://github.com/leonardogbxv" target="_blank">
           <Image
             src="https://github.com/leonardogbxv.png"
             alt="Leonardo's GitHub Profile"
             width={300}
             height={300}
           />
-        </a>
+        </Link>
 
         <p>
           <span>Olá! Meu nome é Leonardo.&nbsp;</span>
-          <span>Sou um desenvolvedor, focado em desenvolvimento mobile.</span>
+          <span>Sou um desenvolvedor mobile apaixonado por tecnologia.</span>
         </p>
 
         <p className={styles.socials}>
-          <a href="https://www.linkedin.com/in/leonardo-g-batista/" target="_blank">
+          <Link href="https://www.linkedin.com/in/leonardo-g-batista/" target="_blank">
             <FontAwesomeIcon
               icon={faLinkedin}
               size="2x"
             />
-          </a>
+          </Link>
 
-          <a href="https://github.com/leonardogbxv" target="_blank">
+          <Link href="https://github.com/leonardogbxv" target="_blank">
             <FontAwesomeIcon
               icon={faGithub}
               size="2x"
             />
-          </a>
+          </Link>
 
-          <a href="mailto:leonardog.bat@gmail.com" target="_blank">
+          <Link href="mailto:leonardog.bat@gmail.com" target="_blank">
             <FontAwesomeIcon
               icon={faEnvelope}
               size="2x"
             />
-          </a>
+          </Link>
         </p>
       </section>
 
