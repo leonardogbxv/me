2bebc4b45fdbae5ce3280684bf4fa87e4c98efba
Leonardo Gomes <leonardog.bat@gmail.com>
2024-11-05 02:43:17 -0300

    refactor: use `2-digit` day config

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,9 +2,9 @@ export function formatISODateToLocaleString(isoDate: string) {
   const date = new Date(isoDate);
   const locale: Intl.LocalesArgument = 'pt-BR';
 
-  const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
-  const monthName = date.toLocaleDateString(locale, options);
+  const options: Intl.DateTimeFormatOptions = { month: 'long', day: '2-digit' };
+  const dayMonth = date.toLocaleDateString(locale, options);
   const year = date.getFullYear();
 
-  return `${monthName}, ${year}`;
+  return `${dayMonth}, ${year}`;
 }
