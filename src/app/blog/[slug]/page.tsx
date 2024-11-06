a3b4781a70193418f103ca0e6c1686ccaacd8979
Leonardo Gomes <leonardog.bat@gmail.com>
2024-11-05 22:40:28 -0300

    feat: add `Comments` component and use `generateMetadata` to dynamically change the page head title

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,29 @@
 import { formatISODateToLocaleString } from '@/utils/date';
 import { getPostBySlug, getSlugs } from '@/utils/blogFetchers';
 import styles from './page.module.css';
+import { Metadata } from 'next';
+import Comments from '@/components/Comments/Comments';
+
+type BlogPostParams = {
+  params: { slug: string };
+};
 
 export async function generateStaticParams() {
   return getSlugs();
 }
 
-export default async function BlogPost({ params }: { params: { slug: string } }) {
+export async function generateMetadata(
+  { params }: BlogPostParams,
+): Promise<Metadata> {
+  const post = await getPostBySlug(params.slug);
+
+  return {
+    title: post.metadata.title,
+    description: post.metadata.description
+  };
+}
+
+export default async function BlogPost({ params }: BlogPostParams) {
   const post = await getPostBySlug(params.slug);
 
   return (
@@ -19,6 +36,8 @@ export default async function BlogPost({ params }: { params: { slug: string } })
       </header>
 
       {post.content}
+
+      <Comments />
     </article>
   );
 }
