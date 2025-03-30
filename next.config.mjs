60f7fe54e7ea2a9c562c0dc782585e7399dde87a
Leonardo Gomes <leonardog.bat@gmail.com>
2025-03-30 18:54:24 -0300

    fix: remove unnecessary configuration of mdx files

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,3 @@
-import createMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
-import rehypePrettyCode from 'rehype-pretty-code';
-import mdxMermaid from 'mdx-mermaid';
-
-/** @type {import('rehype-pretty-code').Options} */
-const options = {
-  theme: 'dracula',
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
@@ -21,11 +11,4 @@ const nextConfig = {
   },
 };
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [remarkGfm, [mdxMermaid.default, {output: 'svg'}]],
-    rehypePlugins: [[rehypePrettyCode, options]],
-  },
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
